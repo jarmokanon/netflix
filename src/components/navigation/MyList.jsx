6fd9726cee344906ca4416671cbc632513ce1f9d
jarmokanon <62425744+jarmokanon@users.noreply.github.com>
2021-03-14 16:58:32 +0100

    les 2 navbar

---
+++ src/components/navigation/MyList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MyList = () => {
+    return (
+        <div className="container">
+            <div className="left">
+                <h4>My List</h4>
+            </div>
+            <div className="left">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, natus quod corrupti aspernatur, veniam accusantium molestias autem harum impedit possimus commodi? Officiis iure quasi natus molestias voluptatibus in cupiditate maiores?</p>
+            </div>
+        </div>
+    )
+}
+
+export default MyList;
---
+++ src/components/navigation/Series.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Series = () => {
+    return (
+        <div className="container">
+            <div className="left">
+                <h4>Series</h4>
+            </div>
+            <div className="left">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, natus quod corrupti aspernatur, veniam accusantium molestias autem harum impedit possimus commodi? Officiis iure quasi natus molestias voluptatibus in cupiditate maiores?</p>
+            </div>
+        </div>
+    )
+}
+
+export default Series;
