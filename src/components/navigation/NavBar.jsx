3c633ac406d2b6c96fa0e06673f773c99600d0ea
jarmokanon <62425744+jarmokanon@users.noreply.github.com>
2021-03-25 11:14:18 +0100

    navbar gemaakt

--- src/components/navigation/NavBar.jsx
+++ src/components/navigation/NavBar.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import '../../css/App.css';
+import { NavLink } from 'react-router-dom';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faGift } from '@fortawesome/free-solid-svg-icons';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 const NavBar = () => {
@@ -7,14 +13,31 @@ const NavBar = () => {
         <nav className="nav-wrapper red darken-3">
             <div className="container">
                 <a className="brand-logo left">Netflix</a>
-                <ul className="right">
+                <ul className="links left">
                 <li><NavLink to="/">Home</NavLink></li>
                 <li><NavLink to="/series">Series</NavLink></li>
                 <li><NavLink to="/films">Films</NavLink></li>
                 <li><NavLink to="/newandpopular">New and Popular</NavLink></li>
                 <li><NavLink to="/mylist">My List</NavLink></li>
-
                 </ul>
+                <div className="navright right">
+                    <div className="search">
+                        <ul>
+                            <li>
+                                <NavLink to=""><FontAwesomeIcon icon={faSearch} /></NavLink>
+                            </li>
+                            <li>
+                                <NavLink to=""><FontAwesomeIcon icon={faGift} /></NavLink>
+                            </li>
+                            <li>
+                                <NavLink to=""><FontAwesomeIcon icon={faBell} /></NavLink>
+                            </li>
+                            <li>
+                                <NavLink to=""><FontAwesomeIcon icon={faUser} /></NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </nav>
     )
